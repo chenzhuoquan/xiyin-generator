d4171662cae6f5d4dfe8a1025e4fe7095bfad4d7
chenzq <chenzhuoquan@gdyhsp.com>
2026-01-19 21:20:17 +0800

    version2.1 : 用户模块开发完成(注册、登录、注销等)

---
+++ src/main/java/com/xiyin/aicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.xiyin.aicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.xiyin.aicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author xiyin-generator
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
